655ed1023b6de03168ef7726583bf0876c7ddfdd
Ayokunnumi1 <omololuayk@gmail.com>
2023-06-20 08:59:06 +0100

    Edit main.js and body elements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const closeIcon = document.querySelector(".close-container");
 const navLink = document.querySelector(".nav-link");
-const hamburger = document.querySelector(".toolbar-hamburger")
+const hamburger = document.querySelector(".toolbar-hamburger");
+const body = document.getElementsByTagName('body')[0];
 
 // const menuClose = () => { 
 //     closeIcon.style.display = 'none';
@@ -16,14 +17,17 @@ const hamburger = document.querySelector(".toolbar-hamburger")
 // }
 
 const menuOpen = () => {
-        navLink.classList.remove("hide-link")  
+    navLink.classList.remove("hide-link") 
+    body.style = 'overflow: hidden;'
     }
 const menuClose = () => {
     navLink.classList.add("hide-link")
+    body.style = '';
 }
 
 closeIcon.addEventListener("click", menuClose);
 hamburger.addEventListener("click", menuOpen);
+navLink.addEventListener("click",menuClose)
 
 
 
