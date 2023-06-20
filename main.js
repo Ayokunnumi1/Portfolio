a964a8af7a7fe05607762e79611f17f7924ab89a
Ayokunnumi1 <omololuayk@gmail.com>
2023-06-20 20:22:15 +0100

    Add blur to header section for mobile menu

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const closeIcon = document.querySelector('.close-container');
 const navLink = document.querySelector('.nav-link');
 const hamburger = document.querySelector('.toolbar-hamburger');
-const body = document.getElementsByTagName('body')[0];
+const headerSection = document.querySelector('.header-hero-section');
+const toolBarSection = document.querySelector('.toolbar-section > a');
 
 const menuOpen = () => {
   navLink.classList.remove('hide-link');
-  body.style = 'overflow: hidden;';
+  headerSection.classList.add('blur-header');
+  toolBarSection.classList.add('blur-logo');
+  hamburger.classList.add('blur-hamburger');
 };
 const menuClose = () => {
   navLink.classList.add('hide-link');
-  body.style = '';
+  headerSection.classList.remove('blur-header');
+  toolBarSection.classList.remove('blur-logo');
+  hamburger.classList.remove('blur-hamburger');
 };
 
 closeIcon.addEventListener('click', menuClose);
 hamburger.addEventListener('click', menuOpen);
-navLink.addEventListener('click', menuClose);
